e0cd86f942eb295a2eea2d01973a2475444f2e07
clouddmitri <llpandres@gmail.com>
2025-03-12 14:57:12 +0800

    fix(frontend): added workout page with api

--- front-end/flutter_joggernaut/lib/widgets/input_dialog.dart
+++ front-end/flutter_joggernaut/lib/widgets/input_dialog.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputHelper {
   static void showInputDialog(BuildContext context, String title, String hint, TextEditingController controller, VoidCallback callback, TextInputType inputType) {
@@ -48,6 +49,106 @@ class InputHelper {
       content: TextField(
         controller: controller,
         keyboardType: inputType,
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: TextStyle(
+            fontSize: width * 0.03,
+            color: Color.fromRGBO(51, 51, 51, 1),
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w400,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide:  BorderSide(color: Color.fromRGBO(51, 51, 51, 1), width: 0.75),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide:  BorderSide(color: Color.fromRGBO(51, 51, 51, 1), width: 1.25),
+          ),
+        ), 
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          style: TextButton.styleFrom(
+            foregroundColor: Color.fromRGBO(51, 51, 51, 1),
+          ),
+          child: Text(
+            "Cancel",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w400,
+              fontSize: width * 0.04,
+              color: Color.fromRGBO(51, 51, 51, 1)
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            callback(); 
+            Navigator.pop(context); 
+          },
+          style: TextButton.styleFrom(
+            foregroundColor: Color.fromRGBO(51, 51, 51, 1),
+          ),
+          child: Text(
+            "Save",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w400,
+              fontSize: width * 0.04,
+              color: Color.fromRGBO(51, 51, 51, 1)
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+   static void showInputNumDialog(BuildContext context, String title, String hint, TextEditingController controller, VoidCallback callback) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+  
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return InputNumDialog(
+          context: context,
+          title: title,
+          hint: hint,
+          controller: controller,
+          callback: callback,
+          height: screenHeight,
+          width: screenWidth, 
+        );
+      },
+    );
+  }
+  
+  static AlertDialog InputNumDialog({
+    required BuildContext context,
+    required String title,
+    required String hint,
+    required TextEditingController controller,
+    required VoidCallback callback,
+    required double height,
+    required double width
+  }) {
+    return AlertDialog.adaptive(
+      backgroundColor:  Color.fromARGB(255, 255, 255, 255),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'Big Shoulders Display',
+          fontSize: width * 0.08,
+          fontWeight: FontWeight.bold,
+          color:  Color.fromRGBO(51, 51, 51, 1),
+        ),
+        ), 
+      content: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly], 
         decoration: InputDecoration(
           hintText: hint,
           hintStyle: TextStyle(
